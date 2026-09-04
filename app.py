f72fff92f5d52ba436946b973a0c45f7a210b1de
dadaozei888 <dadaozei@tutamail.com>
2026-09-04 18:34:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import requests
 from seleniumbase import SB
 
 # 从环境变量获取账号密码和 TG 配置
-EMAIL        = os.environ.get("KATABUMP_EMAIL") or ""    # 登录邮箱
-PASSWORD     = os.environ.get("KATABUMP_PASSWORD") or "" # 账号密码
+EMAIL        = os.environ.get("lsdxch@protonmail.com") or ""    # 登录邮箱
+PASSWORD     = os.environ.get("6O;r6uuJ9+VOe1=^Ty") or "" # 账号密码
 TG_CHAT_ID   = os.environ.get("TG_CHAT_ID") or ""        # tg通知 chat id(可选)
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") or ""      # tg通知bot token(可选)
 
